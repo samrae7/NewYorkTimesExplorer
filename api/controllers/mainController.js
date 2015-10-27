d262c4b905293b67ea3ff7dcc5ce2d7d2ce7e87c
Sam Rae <samuelrae@gmail.com>
2015-10-27 18:36:08 +0000

    Revert "Adding keyword filter"

--- api/controllers/mainController.js
+++ api/controllers/mainController.js
@@ -1,7 +1,7 @@
 var request = require('request')
 
-function getArticlesWithoutKeyword(req,res){
-  request('http://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date='+ req.params.date + '&end_date=' + req.params.date + '&api-key=' + process.env.NYT_API_KEY, function (error, response, body) {
+function getArticles(req,res){
+  request('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=presidential+election&begin_date=20120901&end_date=20120931&api-key=' + process.env.NYT_API_KEY, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         console.log(JSON.parse(body))
         var data = JSON.parse(body)
@@ -10,10 +10,10 @@ function getArticlesWithoutKeyword(req,res){
   })
 }
 
-function getArticles(req,res){
+function getArticlesByDate(req,res){
   console.log(req.params.date)
   var date=req.params.date
-  request('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + req.params.keyword + '&begin_date='+ date + '&end_date=' + date + '&api-key=' + process.env.NYT_API_KEY, function (error, response, body) {
+  request('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=presidential+election&begin_date='+ date + '&end_date=' + date + '&api-key=' + process.env.NYT_API_KEY, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         console.log(JSON.parse(body))
         var data = JSON.parse(body)
@@ -30,6 +30,6 @@ function sayHi(req, res) {
 
 module.exports = {
   getArticles: getArticles,
-  getArticlesWithoutKeyword: getArticlesWithoutKeyword,
+  getArticlesByDate: getArticlesByDate,
   sayHi: sayHi
 }
