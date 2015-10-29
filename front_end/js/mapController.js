b51948f4849c2bd71f6348c24b1c06c443f9f361
Sam Rae <samuelrae@gmail.com>
2015-10-29 13:08:11 +0000

    where I left off with Tom. Refactored and got console logs with locations and latlng

--- front_end/js/mapController.js
+++ front_end/js/mapController.js
@@ -4,79 +4,75 @@ angular
 
     // Do stuff with your $scope.
 
-    var self = this
+    var self = this;
 
-    self.locations = []
+    self.Markers = []
 
-    _($rootScope.articles).forEach(function(article) {
-      article.locations = []
+    var geodArticles = _($rootScope.articles).forEach(function(article) {
+        //article.locations = []
       _(article.keywords).forEach(function(keyword) {
 
           if(keyword.name==='glocations') {
-            
+            article.locations = []
             article.locations.push({name: keyword.value})
             console.log('added'+ keyword.value + 'to', article.headline.main)
             } else {
               console.log('no location added!')
             }
-        }).value();
-      if (article.locations[0]) {
-        console.log("'" + article.headline.main + "' has location " + article.locations[0].name)
+        }).value()
+      if (article.locations) {
         var geocoder = new google.maps.Geocoder();
-
         geocoder.geocode( { "address": article.locations[0].name }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-              console.log('ok')
-              article.locations[0].latitude = results[0].geometry.location.lat();
-              article.locations[0].longitude = results[0].geometry.location.lng();
-
-              console.log('article lat', article.locations[0].latitude)
-              console.log('article lng', article.locations[0].longitude)
+          if (status == google.maps.GeocoderStatus.OK && results.length > 0) {console.log('ok')
+            article.locations[0].latitude = results[0].geometry.location.lat();
+            article.locations[0].longitude = results[0].geometry.location.lng();
             }
-
-        });
+          });
       }
     }).value();
 
-     
-
-      _($rootScope.articles).forEach(function(article) {
-
-         // var geocoder = new google.maps.Geocoder();
-
-        console.log('article.locations[0].name', article.locations[0].name)
-
-        geocoder.geocode( { "address": article.locations[0].name }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-              console.log('ok')
-              article.locations[0].latitude = results[0].geometry.location.lat();
-              article.locations[0].longitude = results[0].geometry.location.lng();
-
-              console.log('article lat', article.locations[0].latitude)
-              console.log('article lng', article.locations[0].longitude)
-            }
-
-        });
-      })
-
+    console.log("geodArticles", geodArticles);
 
     // uiGmapGoogleMapApi is a promise.
     // The "then" callback function provides the google.maps object.
-    // uiGmapGoogleMapApi.then(function(maps) {
-    //   $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-    //   $scope.options = {scrollwheel: false};
-    //   $scope.coordsUpdates = 0;
-    //   $scope.dynamicMoveCtr = 0;
-    //   $scope.marker = {
-    //     id: 0,
-    //     coords: {
-    //        latitude: $scope.latitude,
-    //     longitude: $scope.longitude
-    //     }
-    //   }
-    //   console.log($scope.marker)
-    //   $scope.dynamicMoveCtr++;
-    // })
+    uiGmapGoogleMapApi.then(function(maps) {
+
+      $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
+      $scope.options = {scrollwheel: false};
+      $scope.coordsUpdates = 0;
+      $scope.dynamicMoveCtr = 0;
+
+      // $scope.Markers.push(
+      //     {
+      //       id:0,
+      //       latitude:40.1451,
+      //       longitude:-99.6680
+      //     }
+      //   )
+
+      console.log('ARTICLES', $rootScope.articles)
+
+      _($rootScope.articles).forEach(function(article, index) {
+        console.log('ARTICLE', article)
+        // console.log('INDEX', index)
+
+        // if (article.locations[0] != undefined) {
+        //   console.log (article)
+        //   self.Markers.push(
+        //     {
+        //       id: index,
+        //       latitude: article.locations[0].latitude,
+        //       longitude: article.locations[0].longitude
+        //     }
+        //   )
+        // }
+
+      }).value()
+
+      //console.log($scope.Markers)
+      //$scope.dynamicMoveCtr++;
+
+    })
 
 }]);
 
