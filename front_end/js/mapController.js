a020ef6e18ad8536dd8f2fb90d1e7edf7c496e5a
Sam Rae <samuelrae@gmail.com>
2015-10-29 10:02:31 +0000

    got it console logging lat and lng for each article that has a location

--- front_end/js/mapController.js
+++ front_end/js/mapController.js
@@ -14,46 +14,70 @@ angular
 
           if(keyword.name==='glocations') {
             
-            article.locations.push(keyword.value)
+            article.locations.push({name: keyword.value})
             console.log('added'+ keyword.value + 'to', article.headline.main)
             } else {
               console.log('no location added!')
             }
         }).value();
-      console.log('article.locations', article.locations)
+      if (article.locations[0]) {
+        console.log("'" + article.headline.main + "' has location " + article.locations[0].name)
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode( { "address": article.locations[0].name }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+              console.log('ok')
+              article.locations[0].latitude = results[0].geometry.location.lat();
+              article.locations[0].longitude = results[0].geometry.location.lng();
+
+              console.log('article lat', article.locations[0].latitude)
+              console.log('article lng', article.locations[0].longitude)
+            }
+
+        });
+      }
     }).value();
 
-   
+     
 
-      var geocoder = new google.maps.Geocoder();
-      geocoder.geocode( { "address": self.locations[0] }, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-        console.log('ok')
-        $scope.latitude = results[0].geometry.location.lat();
-        $scope.longitude = results[0].geometry.location.lng();
+      _($rootScope.articles).forEach(function(article) {
 
-        console.log($scope.latitude)
-        console.log($scope.longitude)
-      }
+         // var geocoder = new google.maps.Geocoder();
+
+        console.log('article.locations[0].name', article.locations[0].name)
+
+        geocoder.geocode( { "address": article.locations[0].name }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+              console.log('ok')
+              article.locations[0].latitude = results[0].geometry.location.lat();
+              article.locations[0].longitude = results[0].geometry.location.lng();
+
+              console.log('article lat', article.locations[0].latitude)
+              console.log('article lng', article.locations[0].longitude)
+            }
+
+        });
+      })
+
 
     // uiGmapGoogleMapApi is a promise.
     // The "then" callback function provides the google.maps object.
-    uiGmapGoogleMapApi.then(function(maps) {
-      $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-      $scope.options = {scrollwheel: false};
-      $scope.coordsUpdates = 0;
-      $scope.dynamicMoveCtr = 0;
-      $scope.marker = {
-        id: 0,
-        coords: {
-           latitude: $scope.latitude,
-        longitude: $scope.longitude
-        }
-      }
-      console.log($scope.marker)
-      $scope.dynamicMoveCtr++;
-    })
-  });
+    // uiGmapGoogleMapApi.then(function(maps) {
+    //   $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
+    //   $scope.options = {scrollwheel: false};
+    //   $scope.coordsUpdates = 0;
+    //   $scope.dynamicMoveCtr = 0;
+    //   $scope.marker = {
+    //     id: 0,
+    //     coords: {
+    //        latitude: $scope.latitude,
+    //     longitude: $scope.longitude
+    //     }
+    //   }
+    //   console.log($scope.marker)
+    //   $scope.dynamicMoveCtr++;
+    // })
+
 }]);
 
 
