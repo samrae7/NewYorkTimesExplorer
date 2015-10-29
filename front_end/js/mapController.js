a6de1b58af015d0e3757193e47cbd50729b6ede2
Sam Rae <samuelrae@gmail.com>
2015-10-29 14:59:58 +0000

    not sure why but I need to commit before switching branches

--- front_end/js/mapController.js
+++ front_end/js/mapController.js
@@ -2,21 +2,11 @@ angular
   .module('nytApp')
   .controller('MapController', ['$rootScope','$scope','SliderFactory', '$log', '$timeout', 'uiGmapGoogleMapApi',function($rootScope, $scope, SliderFactory,$log, $timeout, uiGmapGoogleMapApi) {
 
-    // Do stuff with your $scope.
-
     var self = this;
 
     self.Markers = []
 
-      // self.Markers.push(
-      //     {
-      //       id:0,
-      //       latitude:40.1451,
-      //       longitude:-99.6680
-      //     }
-      //   )
-
-    var geodArticles = _($rootScope.articles).forEach(function(article, index) {
+    _($rootScope.articles).forEach(function(article, index) {
         //article.locations = []
       _(article.keywords).forEach(function(keyword) {
 
@@ -34,7 +24,7 @@ angular
           if (status == google.maps.GeocoderStatus.OK && results.length > 0) {console.log('ok')
             article.locations[0].latitude = results[0].geometry.location.lat();
             article.locations[0].longitude = results[0].geometry.location.lng();
-
+            //fixed my big problem by pushing the marker from here as opposed to further down
               self.Markers.push(
                 {
                   id: index,
@@ -42,14 +32,10 @@ angular
                   longitude: article.locations[0].longitude
                 }
               )
-            // $rootScope.lo = article.locations[0].longitude
-            // $rootScope.la = article.locations[0].latitude
             }
           });
       }
     }).value();
-
-    console.log("geodArticles", geodArticles);
 
     // uiGmapGoogleMapApi is a promise.
     // The "then" callback function provides the google.maps object.
@@ -69,28 +55,6 @@ angular
       //       longitude:$rootScope.lo
       //     }
       //   )
-
-      console.log('ARTICLES', $rootScope.articles)
-
-      _($rootScope.articles).forEach(function(article, index) {
-        console.log('ARTICLE', article)
-        // console.log('INDEX', index)
-
-        // if (article.locations[0] != undefined) {
-        //   console.log (article)
-        //   self.Markers.push(
-        //     {
-        //       id: index,
-        //       latitude: article.locations[0].latitude,
-        //       longitude: article.locations[0].longitude
-        //     }
-        //   )
-        // }
-
-      }).value()
-
-      //console.log($scope.Markers)
-      //$scope.dynamicMoveCtr++;
 
     })
 
